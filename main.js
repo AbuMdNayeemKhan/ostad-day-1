c1b2d53d87e1ea140eb6f4239c7b029622cca65b
unknown <abumohammadnayemkhan@gmail.com>
2023-07-06 17:34:01 +0600

    learning javascript

---
+++ main.js
@@ -0,0 +1,95 @@
+//document.write("Hello World!"); //way to write or print data for view page useing javascirpt 
+
+// comment => js comment use for write something about this code.
+
+// variables variable ue for saving data as store
+
+//var x = 5;
+//var y = 6;
+
+//var z = x + y;
+
+//console.log(z);
+
+//document.write(z);
+
+// Data type
+
+//var color = "Yellow"; // string type data
+
+//var myNum = 01619981998 // numbers type variables
+
+//var y = true; //boolean type data
+
+//var un; // undefine type variable
+
+//Object example
+//const person = {firstName: "Nayeem", lastName: "Khan", age: "44"};
+
+//console.log(person.age);
+
+//console.log(typeof(color)); // way to cheack data type
+
+// Array type
+
+// var cars = ["Saab", "Volvo", "BMW"];
+
+// console.log(cars[2]);
+
+// javascript operator
+
+///var x = 10;
+//var y = 20;
+
+//var zog = x + y; // Addition
+//var biog = x - y; // Subtraction
+//var gun = x * y; // Division
+//var vag = x / y; // Modulus
+//var  increment = x++ //increment one time
+//var deceremet = x-- // Decerement one time
+// = bobth are same data
+// == for cheack both data is smae?
+// !== 
+//===
+//!===
+//>
+//>
+//console.log(z);
+// && => logical and oporator
+//|| => logical or oporator
+//! logical not oparetor
+// = asignment operator 
+// += Addigion operator
+// -= Subtraction Assignment
+// *= Multiplication Assignment
+// /= divisition Assignment
+// %= Remainder Assignment
+
+//js if else 
+// var x = 10;
+// if(x == 10){
+//     console.log("true");
+// } else {
+//      console.log("false")
+// }
+
+// let array = ["a", "b", "c"];
+// for(var i = 0; i < array.length; i++){
+//     console.log(array[i]);
+// }
+
+// var n = 100;
+// var sum = 0;
+
+// for(var i = 0; i < n; i++){
+//     sum = sum + i;
+// }
+
+// console.log(sum);
+
+// var i = 10;
+
+// while(i<15){
+//     console.log(i);
+//     i++
+// }
